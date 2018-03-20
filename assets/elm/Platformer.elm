f267486d00002698dd97c52aa5d6b886f65309f8
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-03-20 23:01:28 +1100

    Finish 'Phoenix Channel Setup chapter'

--- assets/elm/Platformer.elm
+++ assets/elm/Platformer.elm
@@ -1,8 +1,13 @@
 module Platformer exposing (..)
 
 import AnimationFrame exposing (diffs)
-import Html exposing (Html, div)
+import Html exposing (Html, button, div)
+import Html.Events exposing (onClick)
+import Json.Encode as Encode
 import Keyboard exposing (KeyCode, downs)
+import Phoenix.Channel
+import Phoenix.Push
+import Phoenix.Socket
 import Random
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
@@ -41,6 +46,7 @@ type alias Model =
     , itemPositionX : Int
     , itemPositionY : Int
     , itemsCollected : Int
+    , phxSocket : Phoenix.Socket.Socket Msg
     , playerScore : Int
     , timeRemaining : Int
     }
@@ -55,14 +61,44 @@ initialModel =
     , itemPositionX = 500
     , itemPositionY = 300
     , itemsCollected = 0
+    , phxSocket = initialSocketJoin
     , playerScore = 0
     , timeRemaining = 10
     }
 
 
+initialSocket : ( Phoenix.Socket.Socket Msg, Cmd (Phoenix.Socket.Msg Msg) )
+initialSocket =
+    let
+        devSocketServer =
+            "ws://localhost:4000/socket/websocket"
+    in
+        Phoenix.Socket.init devSocketServer
+            |> Phoenix.Socket.withDebug
+            |> Phoenix.Socket.on "save_score" "score:platformer" SaveScore
+            |> Phoenix.Socket.join initialChannel
+
+
+initialChannel : Phoenix.Channel.Channel msg
+initialChannel =
+    Phoenix.Channel.init "score:platformer"
+
+
+initialSocketJoin : Phoenix.Socket.Socket Msg
+initialSocketJoin =
+    initialSocket
+        |> Tuple.first
+
+
+initialSocketCommand : Cmd (Phoenix.Socket.Msg Msg)
+initialSocketCommand =
+    initialSocket
+        |> Tuple.second
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( initialModel, Cmd.none )
+    ( initialModel, Cmd.map PhoenixMsg initialSocketCommand )
 
 
 
@@ -73,6 +109,10 @@ type Msg
     = NoOp
     | CountdownTimer Time
     | KeyDown KeyCode
+    | PhoenixMsg (Phoenix.Socket.Msg Msg)
+    | SaveScore Encode.Value
+    | SaveScoreError Encode.Value
+    | SaveScoreRequest
     | SetNewItemPositionX Int
     | TimeUpdate Time
 
@@ -139,6 +179,41 @@ update msg model =
                 _ ->
                     ( model, Cmd.none )
 
+        PhoenixMsg msg ->
+            let
+                ( phxSocket, phxCmd ) =
+                    Phoenix.Socket.update msg model.phxSocket
+            in
+                ( { model | phxSocket = phxSocket }
+                , Cmd.map PhoenixMsg phxCmd
+                )
+
+        SaveScore value ->
+            ( model, Cmd.none )
+
+        SaveScoreError message ->
+            Debug.log "Error sending score over socket."
+                ( model, Cmd.none )
+
+        SaveScoreRequest ->
+            let
+                payload =
+                    Encode.object
+                        [ ( "player_score", Encode.int model.playerScore ) ]
+
+                phxPush =
+                    Phoenix.Push.init "save_score" "score:platformer"
+                        |> Phoenix.Push.withPayload payload
+                        |> Phoenix.Push.onOk SaveScore
+                        |> Phoenix.Push.onError SaveScoreError
+
+                ( phxSocket, phxCmd ) =
+                    Phoenix.Socket.push phxPush model.phxSocket
+            in
+                ( { model | phxSocket = phxSocket }
+                , Cmd.map PhoenixMsg phxCmd
+                )
+
         TimeUpdate time ->
             if characterFoundItem model then
                 ( { model
@@ -168,6 +243,7 @@ subscriptions model =
         [ downs KeyDown
         , diffs TimeUpdate
         , every second CountdownTimer
+        , Phoenix.Socket.listen model.phxSocket PhoenixMsg
         ]
 
 
@@ -178,7 +254,17 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ viewGame model ]
+        [ viewGame model
+        , viewSaveScoreButton
+        ]
+
+
+viewSaveScoreButton : Html Msg
+viewSaveScoreButton =
+    div []
+        [ button [ onClick SaveScoreRequest, class "btn btn-primary" ]
+            [ text "Save Score" ]
+        ]
 
 
 viewGame : Model -> Svg Msg
